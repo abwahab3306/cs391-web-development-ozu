96e5f356f17af91c9c841d92f736bf4d15a57050
Abdul Wahab <abdulwahab@pop-os.localdomain>
2020-12-21 00:51:43 +0500

    fixes

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -1,13 +1,11 @@
 
 let loggedIN = sessionStorage.getItem("login");
-// let users = []
-
-///////// LOGIN PAGE ///////////
-
 	if (loggedIN) {
 		setInterval(() => {
 			document.getElementById("loader").style.display = "none";
 		}, 1000);
+	}else{
+		window.location.pathname = "/"
 	}
 
 
@@ -22,20 +20,4 @@ let loggedIN = sessionStorage.getItem("login");
 	<li><a href="course.html">Register courses</a></li>
 	<li><a href="student.html">Courses</a></li>
 	<li><a href="home.html/#about">Contact Us</a></li>
-  </ul>
-
-
-<nav class="home_nav" style="background-color: rgb(32, 94, 94);height: 100px;
-">
-<p><b>-Web Sayfası İçeriği</b> 
-
-<article class ="infhover">
-			<section style="text-decoration: dotted;font-style: italic;font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; ">
-	   
-				 <li class="list">   <b>Avaliable courses For Students</b>   </li>
-			 
-			</section>
-	  </article>
-
-
-</nav>`
+  </ul>`
